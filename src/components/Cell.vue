5ba442bf84d37fba09a82b6d9057424b32ae3379
Michael Griffin <mdgriffin064@gmail.com>
2021-02-08 19:09:02 +0000

    Enabled merging cells horizontally and vertically

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,18 +1,55 @@
 <template>
-    <td :colspan="colspan">
+    <td :colspan="colspan" :rowspan="rowspan">
         Cell {{cellIndex}}
         <slot></slot>
+        <button v-on:click="mergeDown" class="mergeDown"></button>
+        <button v-on:click="mergeRight" class="mergeRight"></button>
     </td>
 </template>
 
 <script>
 export default {
-    props: ['colspan', 'cellIndex']
+    props: ['colspan', 'rowspan', 'rowIndex', 'cellIndex'],
+    methods: {
+        mergeDown() {
+            this.$emit('mergeDown', this.rowIndex, this.cellIndex);
+        },
+        mergeRight() {
+            this.$emit('mergeRight', this.rowIndex, this.cellIndex);
+        }
+    }
 }
 </script>
 
 <style>
 td {
     border: 1px solid red;
+    position: relative;
 }
+
+.mergeDown,
+.mergeRight {
+    position: absolute;
+    border: none;
+    background: transparent;
+    padding: 0;
+}
+
+.mergeDown {
+    height: 2px;
+    bottom: -1px;
+    left: 0;
+    right: 0;
+    width: 100%;
+   cursor: row-resize;   
+}
+
+.mergeRight {
+    width: 2px;
+    top: 0;
+    right: -1px;
+    bottom: 0;
+    cursor: col-resize;  
+}
+
 </style>
--- src/components/TableBuilder.vue
+++ src/components/TableBuilder.vue
@@ -4,11 +4,25 @@
       <Cell
         v-for="(cell, cellIndex) in row"
         :colspan="cell.colspan"
+        :rowspan="cell.rowspan"
+        :rowIndex="rowIndex"
         :cellIndex="cellIndex"
+        v-on:mergeDown="mergeDown"
+        v-on:mergeRight="mergeRight"
         :key="'cell-' + cellIndex"
       >
-        <button v-if="cellIndex == row.length - 1" v-on:click="addCell($event, rowIndex)">+C</button>
-        <button v-if="cellIndex == row.length - 1" v-on:click="addRow($event, rowIndex)">+R</button>
+        <button
+          v-if="cellIndex == row.length - 1"
+          v-on:click="addCell($event, rowIndex)"
+        >
+          +C
+        </button>
+        <button
+          v-if="cellIndex == row.length - 1"
+          v-on:click="addRow($event, rowIndex)"
+        >
+          +R
+        </button>
       </Cell>
     </tr>
   </table>
@@ -35,22 +49,39 @@ export default {
     };
   },
   methods: {
-      addCell(e, rowIndex) {
-          console.log('ddf')
-          this.rows[rowIndex].push({
-              colspan: 1
-          })
-      },
-      addRow(e, rowIndex) {
-          this.rows.splice(rowIndex, 0, this.rows[rowIndex])
-      }
-  }
+    addCell(e, rowIndex) {
+      this.rows.forEach((row) =>
+        row.push({
+          colspan: 1,
+          rowspan: 1
+        })
+      );
+    },
+    addRow(e, rowIndex) {
+      this.rows.splice(rowIndex, 0, this.rows[rowIndex].slice());
+    },
+    mergeRight(rowIndex, cellIndex) {
+        if (cellIndex < this.rows[rowIndex].length - 1) {
+            this.rows[rowIndex].splice(cellIndex + 1, 1);
+            this.rows[rowIndex][cellIndex].colspan++;
+        }
+    },
+    mergeDown(rowIndex, cellIndex) {
+        console.log('merging down', rowIndex, cellIndex);
+        // remove item from row below
+        // change row span to 2
+        if(rowIndex < this.rows.length - 1) {
+            this.rows[rowIndex + 1].splice(cellIndex, 1);
+            this.rows[rowIndex][cellIndex].rowspan++;
+        }
+    },
+  },
 };
 </script>
 
 <style>
 table {
-    border: 1px solid red;
-    border-collapse: collapse;
+  border: 1px solid red;
+  border-collapse: collapse;
 }
 </style>
